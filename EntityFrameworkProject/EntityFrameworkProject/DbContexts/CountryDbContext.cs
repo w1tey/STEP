89a34fd17f7d23f7be095e0b1eca6fd4dfdc44d6
w1tey <moryakovemil@gmail.com>
2023-06-18 18:51:17 +0400

    Hello

---
+++ EntityFrameworkProject/EntityFrameworkProject/DbContexts/CountryDbContext.cs
@@ -0,0 +1,40 @@
+﻿using EntityFrameworkProject.DbModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EntityFrameworkProject.DbContexts
+{
+    public class CountryDbContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            ConfigurationBuilder builder = new();
+
+            builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            string connectionString = configuration.GetConnectionString("LocalConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var country = modelBuilder.Entity<Country>();
+
+            country.HasKey(x => x.Id);
+            country.Property(x => x.Name).IsRequired();
+            country.Property(x => x.Government).IsRequired();
+            country.Property(x => x.ImageURL).IsRequired();
+            country.Property(x => x.Population).IsRequired();
+            country.Property(x => x.Area).IsRequired();
+            country.Property(x => x.GDP).IsRequired();
+        }
+
+
+
+    }
+}
---
+++ EntityFrameworkProject/EntityFrameworkProject/DbModels/Country.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkProject.DbModels
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Government { get; set; }
+        public string ImageURL { get; set; }
+        public int Population { get; set;}
+        public float Area { get; set; }
+        public float GDP { get; set; }
+
+    }
+
+}
