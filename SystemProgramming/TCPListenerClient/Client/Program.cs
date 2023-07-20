8f0f47452abc65a8293d4ded584e3ebefe29a421
w1tey <moryakovemil@gmail.com>
2023-07-20 12:27:52 +0400

    TCP Server and Client program

---
+++ SystemProgramming/TCPListenerClient/Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+try
+{
+    TcpClient Client = new TcpClient();
+    Client.Connect("127.0.0.1", 8080);
+    Console.WriteLine("Connection Successful");
+
+    while (true)
+    {
+        Console.Write("Enter a message to send.\nEnter 0 to exit.\nEnter: ");
+        string Message = Console.ReadLine();
+
+        byte[] byteMessage = Encoding.UTF8.GetBytes(Message);
+        NetworkStream Stream = Client.GetStream();
+        Stream.Write(byteMessage, 0, byteMessage.Length);
+
+        if (Message == "0")
+        {
+            Client.Close();
+            break;
+        }
+    }
+}
+catch (Exception ex) { Console.WriteLine("Exception: " + ex.Message); }
---
+++ SystemProgramming/TCPListenerClient/Listener/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+IPAddress IP = IPAddress.Parse("127.0.0.1");
+int Port = 8080;
+TcpListener listener = new TcpListener(IP, Port);
+
+Console.WriteLine("Waiting for messages.");
+
+try
+{
+    listener.Start();
+
+    while (true)
+    {
+        TcpClient Client = listener.AcceptTcpClient();
+
+        try
+        {
+            using NetworkStream stream = Client.GetStream();
+            byte[] byteBuffer = new byte[1024];
+            int byteCount = stream.Read(byteBuffer, 0, byteBuffer.Length);
+            string Message = Encoding.UTF8.GetString(byteBuffer, 0, byteCount);
+            Console.WriteLine("Message: " + Message);
+        }
+        catch (Exception ex) { Console.WriteLine("Exception: " + ex.Message); }
+        finally { Client.Close(); }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Exception: " + ex.Message);
+}
+finally
+{
+    listener.Stop();
+}
